8615f54fb2824a315cd1ff78cac196f2412e4a3e
Andrei Eskov <ixplo@ya.ru>
2025-02-17 17:42:06 -0500

    Add simple mat components

--- src/app/create-course/create-course-step-1/create-course-step-1.component.ts
+++ src/app/create-course/create-course-step-1/create-course-step-1.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {UntypedFormBuilder, Validators} from '@angular/forms';
+import {MatCalendarCellClassFunction} from "@angular/material/datepicker";
 
+const SAMPLE_TEXT = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum sit amet metus id scelerisque. Mauris nibh felis, porta a cursus a, commodo sed lectus. Ut nibh libero, dictum non ultricies ut, varius ac leo. Mauris bibendum elit non justo volutpat finibus. Sed vel purus mattis, elementum libero et, suscipit purus. Maecenas cursus feugiat purus, vel tristique tortor tempor id. Quisque accumsan ornare porttitor. Praesent dapibus nisi ut lobortis tincidunt. Donec aliquet lobortis turpis, non fringilla dui molestie vel. In maximus, purus non dictum venenatis, mauris dui eleifend mi, a dictum lectus augue sed nunc. Quisque facilisis euismod ligula, vel ultricies quam fermentum vel. Fusce finibus metus quis venenatis convallis. Aenean eu varius felis. Cras suscipit rhoncus felis, eu egestas lacus. Aenean laoreet vehicula purus.'
 
 @Component({
     selector: "create-course-step-1",
@@ -20,8 +22,16 @@ export class CreateCourseStep1Component {
     category: ['BEGINNER', Validators.required],
     courseType: ['premium', Validators.required],
     downloadsAllowed: [false, Validators.requiredTrue],
-    longDescription: ['', [Validators.required, Validators.minLength(3)]]
+    longDescription: [SAMPLE_TEXT, [Validators.required, Validators.minLength(3)]]
   });
+
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    const date = cellDate.getDate()
+    if (view === 'month' && date === 1) {
+      return 'highlight-date'
+    }
+    return ''
+  }
 
   constructor(private fb: UntypedFormBuilder) {
 
